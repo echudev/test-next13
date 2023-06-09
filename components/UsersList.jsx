0240418bfc5e96fa6a18b59cf7032883d50f2d7c
Ezequiel <ezequiel.maranda@gmail.com>
2023-06-09 19:42:59 -0300

    detalles de estilado

--- components/UsersList.jsx
+++ components/UsersList.jsx
@@ -9,7 +9,7 @@ function UsersList({ users }) {
     <div className="flex justify-center flex-wrap gap-6 text-sm select-none text-violet-100">
       {users.map((user) => (
         <div
-          className="flex justify-between items-center rounded-lg bg-opacity-10 bg-violet-400 min-w-[300px] min-h-[100px] p-4 hover:scale-105 transition"
+          className="flex justify-between items-center rounded-lg bg-opacity-10 bg-violet-400 min-w-[300px] min-h-[100px] p-4 hover:scale-105 hover:brightness-125 transition"
           key={user.id}
           onClick={() => {
             router.push(`/users/${user.id}`);
@@ -26,8 +26,8 @@ function UsersList({ users }) {
             <p>Nombre: {user.first_name}</p>
             <p>Apellido: {user.last_name}</p>
           </div>
-          <button className="bg-orange-500 text-orange-100 px-2 rounded-md m-3 h-7 w-12 hover:brightness-125 transition">
-            VER
+          <button className="bg-orange-500 text-orange-200 px-2 rounded-md m-3 h-7 w-12 hover:text-orange-500 hover:bg-orange-300 transition font-bold">
+            Ver
           </button>
         </div>
       ))}
